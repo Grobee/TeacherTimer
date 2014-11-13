1d70fb45a0365ac69b87f95aa36d2528e8a122b7
Grobee <grobi1993@gmail.com>
2014-11-13 21:22:36 +0100

    Added a "Session" class

---
+++ Session.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeacherTimer
+{
+    public class Session
+    {
+        public int HoursDone { get; set; }
+        public int LongestStreak { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime FirstTime { get; set; }
+        public int ElapsedHours { get; set; }
+        public int ElapsedMinutes { get; set; }
+        public bool InProgress { get; set; }
+    }
+}
